7b06d259be459b889784463c9258d2170b2343a3
Santiago Sica <santisica29@gmail.com>
2026-02-05 00:28:30 -0300

    Add Menu

---
+++ FlashcardsRevisited/Views/TableVisualisation.cs
@@ -0,0 +1,11 @@
+﻿using FlashcardsRevisited.Models;
+
+namespace FlashcardsRevisited.Views;
+
+internal class TableVisualisation
+{
+    internal static void ShowTable(List<StackDeck> listOfStacks)
+    {
+        throw new NotImplementedException();
+    }
+}
