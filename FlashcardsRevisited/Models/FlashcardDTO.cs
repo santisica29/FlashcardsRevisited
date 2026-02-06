5d429a40cea0779b5cb30f3fd2710750aa39fb64
Santiago Sica <santisica29@gmail.com>
2026-02-06 13:37:52 -0300

    Create FlashcardDTO

---
+++ FlashcardsRevisited/Models/FlashcardDTO.cs
@@ -0,0 +1,20 @@
+﻿namespace FlashcardsRevisited.Models;
+internal class FlashcardDTO
+{
+    //private static int LastId { get; set; } = 1;
+    public int Id { get; set; } = 1;
+    public string Front { get; set; }
+    public string Back { get; set; }
+
+    public FlashcardDTO()
+    {
+        Id = Id++;
+    }
+
+    public FlashcardDTO(string front, string back)
+    {
+        Id = Id++;
+        Front = front;
+        Back = back;
+    }
+}
